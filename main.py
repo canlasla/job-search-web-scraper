31c2e2c95c3a343bb030efe37a6fbb7d7b940594
canlasla <lancecanlas25@gmail.com>
2020-07-18 13:11:12 -0400

    Scraped a static site

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+
+URL='https://www.monster.ca/jobs/search/?q=Software-Engineer-intern&where=Toronto__2C-Canada-&jobid=b697209f-417b-4e67-8ac5-e19487dc85b0'
+# URL='https://ca.indeed.com/jobs?q=software%20engineer%20intern&l=Toronto%2C%20ON&vjk=4fb9ee159ff25c91'
+page=requests.get(URL)
+
+print(page.content)
