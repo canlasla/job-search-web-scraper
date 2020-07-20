1fce6216e8ce58a6069d254ae88d435361be94bd
canlasla <lancecanlas25@gmail.com>
2020-07-19 21:18:35 -0400

    Added web scraper for indeed.com

--- indeed.py
+++ indeed.py
@@ -16,23 +16,25 @@ def findJobs():
     wb = copy(rb) 
     ws = wb.get_sheet(0) 
 
-    URL='https://www.monster.ca/jobs/search/?q=Software-Engineer-intern&where=Toronto__2C-Canada-&jobid=b697209f-417b-4e67-8ac5-e19487dc85b0'
+    URL='https://ca.indeed.com/jobs?q=software+engineer+intern&l=Toronto%2C+ON'
     page=requests.get(URL)
 
     soup=BeautifulSoup(page.content, 'html.parser')
-    results=soup.find(id='ResultsContainer')
-    jobElements=results.find_all('section', class_='card-content')
+    results=soup.find(id='resultsCol')
+    jobElements=results.find_all('div', class_='result')
 
     for elem in jobElements:
 
-        if((elem.find('h2', string=lambda text: 'engineer' in text.lower()) is None) and (elem.find('h2', string=lambda text: 'developer' in text.lower()) is None)):
+        titleElem = elem.find('a', class_='jobtitle')
+
+        if(('engineer' not in titleElem.text.strip().lower().split(" ")) and ('developer' not in titleElem.text.strip().lower().split(" "))):
             continue
 
         linkElem = elem.find('a')
-        titleElem = elem.find('h2', class_='title')
-        companyElem= elem.find('div', class_='company')
-        locationElem = elem.find('div', class_='location')
-        timeElem =elem.find('time')
+        titleElem = elem.find('a', class_='jobtitle')
+        companyElem= elem.find('span', class_='company')
+        locationElem = elem.find('span', class_='location')
+        timeElem =elem.find('span', class_='date')
 
         if None in (linkElem, titleElem, companyElem, locationElem,timeElem):
             continue
@@ -43,11 +45,19 @@ def findJobs():
         location= locationElem.text.strip()
         time=timeElem.text.strip()
 
+        if(link[0:7]!="/rc/clk"):
+            continue
+
+        newLink=link.replace("/rc/clk", "https://ca.indeed.com/viewjob")
+        cutOff=newLink.index("&")
+        end=len(newLink)-1
+        newLink = newLink[0: cutOff:] + newLink[end + 1::]
+
         ws.write(row, 0, time)
         ws.write(row, 1, title)
         ws.write(row, 2, company)
         ws.write(row, 3, location)
-        ws.write(row, 4, link)
+        ws.write(row, 4, newLink)
         ws.write(row, 5, datetime.now(), dateStyle)
         ws.write(row, 6, 'Not Applied', appliedStyle)
 
